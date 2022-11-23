5b55b746b1a84b41460a929bbdf350aaf1a7be5d
CristianVaghetti <vaghetticristian@gmail.com>
2022-11-23 13:33:16 -0300

    Adicionado corsheaders

--- projeto_camara/settings.py
+++ projeto_camara/settings.py
@@ -27,13 +27,17 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'corsheaders',
     'vereador',
     'fornecedor',
     'material',
     'utilizado',
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
