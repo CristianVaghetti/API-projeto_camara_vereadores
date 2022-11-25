65eb9150d228a14239315c748d684f7abfdd26ca
CristianVaghetti <vaghetticristian@gmail.com>
2022-11-25 17:02:47 -0300

    alterado nome de coluna no banco

--- vereador/models.py
+++ vereador/models.py
@@ -4,7 +4,7 @@ from uuid import uuid4
 class Vereador(models.Model):
     vereador_id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     vereador_matricula = models.CharField(max_length=15, blank=False, null=False)
-    Vereador_nome = models.CharField(max_length=50, blank=False, null=False)
+    vereador_nome = models.CharField(max_length=50, blank=False, null=False)
     vereador_cadastro_data = models.DateField(auto_now_add=True)
 
     class Meta:
