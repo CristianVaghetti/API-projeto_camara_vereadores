6aa1991a9d990843d1d48ace8b0ea0e85a6813fa
CristianVaghetti <vaghetticristian@gmail.com>
2022-12-22 19:24:30 -0300

    alterado related_name dos fornecedores

--- material/models.py
+++ material/models.py
@@ -12,7 +12,7 @@ class TipoMaterial(models.Model):
 class Material(models.Model):
     material_id = models.AutoField(primary_key=True)
     material_tipo = models.ForeignKey(TipoMaterial, on_delete=models.SET_NULL, related_name='material_tipo', null=True)
-    material_fornecedor = models.ForeignKey(Fornecedor, on_delete=models.SET_NULL,  related_name='material_fornecedor', null=True)
+    material_fornecedor = models.ForeignKey(Fornecedor, on_delete=models.SET_NULL,  related_name='fornecedor_detalhe', null=True)
     material_descricao = models.CharField(max_length=50)
     material_valor = models.CharField(max_length=50)
     material_quantidade = models.IntegerField()
